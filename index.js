4cd7ee42e43bef8ab626f9a74f3feb0ef3fe8de1
captivating-form-0787 <avertrees447@gmail.com>
2019-05-09 19:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // add solution here
 
 
-
-
 function theBeatlesPlay(musicians, instruments){
  var beatlesPlay = [];
- for (var i = 0, i<musicians.length, i++){
+ for (var i = 0; i<musicians.length; i++){
    beatlesPlay[i] == musician[i] + " plays the " instruments[i] + ".";
  }
  return beatlesPlay;
@@ -16,17 +14,17 @@ function johnLennonFacts(){
 //array of facts about John Lennon (note that it might not be exactly the
 //following facts):
 
-```js
-const facts = [
-"He was the last Beatle to learn to drive",
-"He was never a vegetarian",
-"He was a choir boy and boy scout",
-"He hated the sound of his own voice"
-];
-```
-return 0
+//```js
+//const facts = [
+//"He was the last Beatle to learn to drive",
+//"He was never a vegetarian",
+//"He was a choir boy and boy scout",
+//"He hated the sound of his own voice"
+//];
+//```
+return 0;
 }
 
 function iLoveTheBeatles(){
-  return 0  
+  return 0;
 }
