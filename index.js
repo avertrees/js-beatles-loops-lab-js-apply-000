0eae2e3920e2f320df068b16991e409422679899
captivating-form-0787 <avertrees447@gmail.com>
2019-05-09 19:36:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,8 @@
 
 
 function theBeatlesPlay(){
- var musicians[] = {}
- var instruments[] = {}
-
-// //The function should also contain a for loop
-// which loops over the array of musicians. You'll want to be careful about what
-// value you set your counter variable to store. (Hint: Think about what the first
-// index of an array is). The first time through the loop, the body of the loop
-// should create a string using the first index of the musicians array and the
-// first index of the instruments array: `"John Lennon plays guitar"`. This string
-// should be added to the empty array you created. The loop should make the same
-// sentence for every member of the musicians array. The function should return the
-// array of new strings. 
+ var beatlesPlay[] = {}
+  
 }
 
 function johnLennonFacts(){
