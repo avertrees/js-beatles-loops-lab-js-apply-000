278534bdee529b7129b5f13a926b898250943693
captivating-form-0787 <avertrees447@gmail.com>
2019-05-09 19:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,15 @@ function johnLennonFacts(facts){
 }
 
 function iLoveTheBeatles(n){
+var ilovethebeatles = [];
+do{
   
+} while();
+//create a variable that stores an empty array. 
+//Then, implement a do-while loop inside the function that adds `"I love the Beatles!"`
+//to the empty array. 
+//Then the loop should increment the number passed in as a
+parameter. The condition of the loop should check to see that the parameter
+number is less than `15`. The function should return the array with the strings
+`"I love the Beatles!"`.
 }
