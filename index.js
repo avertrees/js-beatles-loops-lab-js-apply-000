9eb995660127ddd2ef1d1b3b9c8b6804b0961a78
captivating-form-0787 <avertrees447@gmail.com>
2019-05-09 19:40:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(){
-  + Create a function `johnLennonFacts`. This function will accept one argument, an
++ Create a function `johnLennonFacts`. This function will accept one argument, an
 array of facts about John Lennon (note that it might not be exactly the
 following facts):
 
 ```js
 const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
+"He was the last Beatle to learn to drive",
+"He was never a vegetarian",
+"He was a choir boy and boy scout",
+"He hated the sound of his own voice"
 ];
 ```
 }
